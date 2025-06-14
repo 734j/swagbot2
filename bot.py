fe6ab3524b5e3211879c95965d4f1b6b3bb89f9f
Dainis <gatesby@protonmail.com>
2025-06-14 10:45:15 +0300

    pitting: fixed indentation issue II

--- bot.py
+++ bot.py
@@ -321,12 +321,12 @@ async def pit(interaction: discord.Interaction, user: discord.Member, reason: st
             await channel.send(f"{user.mention} ({user}) was pitted by {interaction.user.mention} for reason: {reason}.")
             await pit.send (f"A loud thud shakes the depths of the Pit as {user.mention} ({user}) falls to the ground... Welcome your new friend.")
 	    elif reason == "":
-                await generic_pit(interaction, user)
-                channel = bot.get_channel(channel_pplofthepit)
-                await interaction.response.send_message(f"{user.mention} has been pitted.\n{pitted_gif}")
-                await user.send(f'You have been pitted in 69SwagBalls420 cord for undisclosed reasons.')
-                await channel.send(f"{user.mention} ({user}) was pitted by {interaction.user.mention} for unknown reasons! :DEVIL:")
-                await pit.send (f"A loud thud shakes the depths of the Pit as {user.mention} ({user}) falls to the ground... Welcome your new friend.")
+            await generic_pit(interaction, user)
+            channel = bot.get_channel(channel_pplofthepit)
+            await interaction.response.send_message(f"{user.mention} has been pitted.\n{pitted_gif}")
+            await user.send(f'You have been pitted in 69SwagBalls420 cord for undisclosed reasons.')
+            await channel.send(f"{user.mention} ({user}) was pitted by {interaction.user.mention} for unknown reasons! :DEVIL:")
+            await pit.send (f"A loud thud shakes the depths of the Pit as {user.mention} ({user}) falls to the ground... Welcome your new friend.")
         else:
             await interaction.response.send_message("https://cdn.discordapp.com/attachments/1239258065988222999/1261509266208981073/RDT_20240712_2224291177474633641757631.jpg?ex=6696834e&is=669531ce&hm=b441f6ee1d35f9e6e00823f493b26e7c859377ddf5a6f7c1930cb5ee7d21bcc8&.", ephemeral=True)
     except discord.Forbidden:
